1b0393a52a5101d3c47b725b807cbd88c454793d
优雅 <13264305+Elejiang@user.noreply.gitee.com>
2024-04-01 21:22:19 +0800

    完成user组件库的封装

---
+++ frameworks/user/src/main/java/com/grace/train12306/framework/starter/user/config/UserAutoConfiguration.java
@@ -0,0 +1,28 @@
+package com.grace.train12306.framework.starter.user.config;
+
+import com.grace.train12306.framework.starter.user.core.UserTransmitFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import static com.grace.train12306.framework.starter.bases.constant.FilterOrderConstant.USER_TRANSMIT_FILTER_ORDER;
+
+
+/**
+ * 用户配置自动装配
+ */
+@ConditionalOnWebApplication
+public class UserAutoConfiguration {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(USER_TRANSMIT_FILTER_ORDER);
+        return registration;
+    }
+}
---
+++ frameworks/user/src/main/java/com/grace/train12306/framework/starter/user/core/UserInfoDTO.java
@@ -0,0 +1,36 @@
+package com.grace.train12306.framework.starter.user.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 用户信息实体
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserInfoDTO {
+
+    /**
+     * 用户 ID
+     */
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 用户 Token
+     */
+    private String token;
+}
