f2cb2aeed44c7aad9b776194e918f43945c7ab18
优雅 <13264305+Elejiang@user.noreply.gitee.com>
2024-04-02 15:04:53 +0800

    完成web组件库的封装

---
+++ frameworks/web/src/main/java/com/grace/train12306/framework/starter/web/config/WebAutoConfiguration.java
@@ -0,0 +1,48 @@
+package com.grace.train12306.framework.starter.web.config;
+
+import com.grace.train12306.framework.starter.web.GlobalExceptionHandler;
+import com.grace.train12306.framework.starter.web.initialize.InitializeDispatcherServletController;
+import com.grace.train12306.framework.starter.web.initialize.InitializeDispatcherServletHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Web 组件自动装配
+ */
+public class WebAutoConfiguration {
+
+    public final static String INITIALIZE_PATH = "/initialize/dispatcher-servlet";
+
+    @Bean
+    @ConditionalOnMissingBean
+    public GlobalExceptionHandler globalExceptionHandler() {
+        return new GlobalExceptionHandler();
+    }
+
+    @Bean
+    public InitializeDispatcherServletController initializeDispatcherServletController() {
+        return new InitializeDispatcherServletController();
+    }
+
+    @Bean
+    public RestTemplate simpleRestTemplate(ClientHttpRequestFactory factory) {
+        return new RestTemplate(factory);
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);
+        factory.setConnectTimeout(5000);
+        return factory;
+    }
+
+    @Bean
+    public InitializeDispatcherServletHandler initializeDispatcherServletHandler(RestTemplate simpleRestTemplate, ConfigurableEnvironment configurableEnvironment) {
+        return new InitializeDispatcherServletHandler(simpleRestTemplate, configurableEnvironment);
+    }
+}
