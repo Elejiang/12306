41334c78901d349385b656bef000d1f3a7641e79
优雅 <elegance520@163.com>
2024-04-05 10:52:22 +0800

    新增ticketservice模块

---
+++ services/ticket-service/src/main/java/com/grace/train12306/biz/ticketservice/TicketServiceApplication.java
@@ -0,0 +1,20 @@
+package com.grace.train12306.biz.ticketservice;
+
+import cn.hippo4j.core.enable.EnableDynamicThreadPool;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 购票服务应用启动器
+ */
+@SpringBootApplication
+@EnableDynamicThreadPool
+@MapperScan("com.grace.train12306.biz.ticketservice.dao.mapper")
+public class TicketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketServiceApplication.class, args);
+    }
+}
